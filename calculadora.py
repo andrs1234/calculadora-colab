d888d0bd69910b3b71060cea8c0c3f08e0298df3
andrs1234 <andybenaventeqro@gmail.com>
2025-04-11 22:16:47 -0600

    agrega interfaz y funciones

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,17 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b if b != 0 else 'Error: la división por cero xd'
+
+def mostrar_menu():
+    print("\nCalculadora colaborativa")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
