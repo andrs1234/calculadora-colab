642ecb8eeb0d9a6fca471b90b3ba2d8cab831b80
andrs1234 <andybenaventeqro@gmail.com>
2025-04-11 22:41:56 -0600

    añade codigo completo

--- calculadora.py
+++ calculadora.py
@@ -3,12 +3,9 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
+
 def multiplicacion(a, b):
     return a * b
-  
-def division(a, b):
-    return a / b if b != 0 else 'Error: división por cero'
-
 
 def division(a, b):
     return a / b if b != 0 else 'Error: la división por cero xd'
